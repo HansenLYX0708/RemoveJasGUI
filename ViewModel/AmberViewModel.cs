cf4e12f50b0f89454a176c22237aff4c573e2854
HansenLYX0708 <1195857096@qq.com>
2018-08-21 14:41:58 +0800

    Add Model project
    1.Add model project
    2.process complie error

---
+++ ViewModel/AmberViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ViewModel
+{
+    class AmberViewModel
+    {
+
+        #region Fields
+        private string _MachineStatus;
+        private string _MonitorMode;
+        #endregion Fields
+
+        #region Constructors
+        public AmberViewModel()
+        {
+
+        }
+        #endregion Constructors
+
+        #region Properties
+        string MachineNum { get; set; }
+        string MachineStatus
+        {
+            get
+            {
+                return _MachineStatus;
+            }
+            set
+            {
+                _MachineStatus = value;
+            }
+        }
+        string MonitorMode
+        {
+            get
+            {
+                return _MonitorMode;
+            }
+            set
+            {
+                _MonitorMode = value;
+            }
+        }
+        #endregion Properties
+
+        #region Methods
+
+        #endregion Methods
+    }
+}
